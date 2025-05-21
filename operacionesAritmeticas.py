2524e86647dc40e8d8e2cd3f7ff42c5c5cf3100a
yavorski95 <yavorski35@gmail.com>
2025-05-21 00:33:37 -0300

    la concha de la lora

---
+++ operacionesAritmeticas.py
@@ -0,0 +1,29 @@
+#Operaciones Aritmeticas:
+
+# SUMA +
+# RESTA -
+# MULTIPLICACION *
+# DIVISION /
+# POTENCIA **
+# RESTO DE LA DIVISION %
+
+#OperacionesLogicas
+
+#Devuelven Verdero o Falso. True , False
+
+# 1 == 2   #Igualdad
+
+# 1 != 2   #Desigual
+
+# 1 < 2    #Menor
+
+# 1 > 2    #Mayor
+
+# 1 <= 2   #Menor o Igual
+
+# 1 >= 2   #Mayor o Igual
+
+
+# Ejercicio 1: crear una variable "multiplicacion" que tenga una multiplicacion entre 2 numeros y despues imprimirla con Print
+# Ejercicio 2: imprimir un ejemplo con igualdad, otro ejemplo con mayor y otro ejemplo con menor o igual
+#
---
+++ variables.py
@@ -0,0 +1,30 @@
+numero = 1   
+
+nombre = "juan"
+
+color = "rojo"
+
+#Ejercicio 1: imprimir las 3 variables
+#Ejercicio 2: concatenar las 3 variables. ej: "el numero es:" , "el nombre es: " , "el color es:"
+#Ejercicio 3: Subir a git
+
+
+
+
+print(nombre, color, numero)
+
+
+print("el numero es", numero)
+
+print("el nombre es", nombre)
+
+print("el color es", color)
+
+edad = numero
+
+print(edad)
+
+
+
+
+
